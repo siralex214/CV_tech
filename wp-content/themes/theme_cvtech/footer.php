cb0aee744ad5ebead0dfbc79c4ec6e42f873d034
Hyotre <72597664+Hyotre1@users.noreply.github.com>
2022-03-15 14:58:17 +0100

    mention

--- wp-content/themes/theme_cvtech/footer.php
+++ wp-content/themes/theme_cvtech/footer.php
@@ -11,22 +11,6 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme_cvtech' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'theme_cvtech' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme_cvtech' ), 'theme_cvtech', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
---
+++ wp-content/themes/theme_cvtech/template_mention.php
@@ -0,0 +1,106 @@
+<?php 
+/* Template Name: mention */
+get_header();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title> bertolucci</title>
+</head>
+<?php
+?>
+<main>
+
+    <ol>
+        <div class=ruban>
+            <h1>Mentions Légales</h1>
+        </div>
+        <h3>
+            <li>Editeur du Site :</li>
+        </h3>
+        <p>SARL sm Numéro de SIRET : 75221735600019</p>
+        <p> Responsable éditorial : bertolucci </p>
+        <p>30 Pl. Henri Gadeau de Kerville, 76100 Rouen > <br>
+            Téléphone : 0360455713</p>
+        <p>Fax : 0972249854</p>
+        <p> Email : contact@bertolucci.fr</p>
+        <h3>
+            <li>Hébergement :</li>
+        </h3>
+        <p> Hébergeur : SARL bertolucci</p>
+        <p>30 Pl. Henri Gadeau de Kerville, 76100 Rouen </p> <br>
+        <h3>Développement :</li>
+        </h3>
+        <p>SARL bertolucci</p>
+        <p> Adresse : 30 Pl. Henri Gadeau de Kerville, 76100 Rouen</p>
+    </ol>
+
+    <div class="mention">
+        <ol>
+            <h3>Conditions d’utilisation :</h3>
+            <h3>
+                <li>Conditions lié au RGPD :</li>
+            </h3>
+            <p>toute informations collecter lors de votre inscription n'est qu'a but purement statistiques.
+                Ces données ne seront utilisé que par bertolucci et ne seront donc vendu d'aucune façon que ce soit
+                Ces données seront stocker pendant 5 ans aprés quoi, Nous effacerons manuellement les données
+                Vous possedez le droit de demandé à supprimer vos données.il sera demandé a la personne une piece d'identité légal pour qu'elle puisse avoir accés a ces informations.
+                Le droit d'accés:vous pouvez avoir accés a vos données collecter en écrivant à l'adresse électronique. Dans ce cas, il sera demandé a la personne une piece d'identité légal pour qu'elle puisse avoir accés a ces informations.
+                Vous obtenez aussi le droit de refuser le traitement de ces données, pour cela veuillez nous envoyez un mail pour nous faire parvenir votre décision
+                vous possedez un dernier droit en accords avec les RGPD. Vous pouvez demander à collecter l'ensemble de vos données pour pouvoir les transmettres a un autre site de collecte de donnée </p>
+            <p>Ce site est proposé en différents langages web (HTML, HTML5, CSS, PHP, etc.…) pour un meilleur confort d’utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
+                L’agence web bertolucci met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L’internaute devra donc s’assurer de l’exactitude des informations auprès de , et signaler toutes modifications du site qu’il jugerait utile. n’est en aucun cas responsable de l’utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
+            <h3>
+                <li>Cookies :</li>
+            </h3>
+            <p>Le site peut-être amené à vous demander l’acceptation des cookies pour des besoins de statistiques et d’affichage. Un cookies est une information déposée sur votre disque dur par le serveur du site que vous visitez. Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple fichier texte auquel un serveur accède pour lire et enregistrer des informations. Certaines parties de ce site ne peuvent être fonctionnelles sans l’acceptation de cookies.</p>
+            <h3>
+                <li>Liens hypertextes :</li>
+            </h3>
+            <p>Les sites internet peuvent offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet. SARL bertolucci ne dispose d’aucun moyen pour contrôler les sites en connexion avec ses sites internet. Ne répond pas de la disponibilité de tels sites et sources externes, ni ne la garantit. Elle ne peut être tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent pleinement à l’internaute, qui doit se conformer à leurs conditions d’utilisation.
+                Les utilisateurs, les abonnés et les visiteurs des sites internet ne peuvent pas mettre en place un hyperlien en direction de ce site sans l’autorisation expresse et préalable de SARL bertolucci.
+                Dans l’hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des sites internet de SARL bertolucci, il lui appartiendra d’adresser un email accessible sur le site afin de formuler sa demande de mise en place d’un hyperlien. SARL bertolucci se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.</p>
+            <h3>
+                <li>Services fournis :</li>
+            </h3>
+            <p>L’ensemble des activités de la société ainsi que ses informations sont présentés sur notre site </a>.
+                SARL bertolucci s’efforce de fournir sur le des informations aussi précises que possible. les renseignements figurant sur le site ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, toutes les informations indiquées sur le site sont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis.</p>
+            <h3>
+                <li>Limitations contractuelles sur les données :</li>
+            </h3>
+            <p>Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse contact@bertolucci.fr, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).
+                Tout contenu téléchargé se fait aux risques et périls de l’utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d’un quelconque dommage subi par l’ordinateur de l’utilisateur ou d’une quelconque perte de données consécutives au téléchargement. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.
+                Les liens hypertextes mis en place dans le cadre du présent site internet en direction d’autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de SARL bertolucci.</p>
+            <h3>
+                <li>Propriété intellectuelle :</li>
+            </h3>
+            <p>Tout le contenu du présent site , incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs.
+                Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l’accord exprès par écrit de SARL bertolucci. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.</p>
+            <h3>
+                <li>Déclaration à la CNIL :</li>
+            </h3>
+            <p>Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l’égard des traitements de données à caractère personnel) relative à l’informatique, aux fichiers et aux libertés, ce site a fait l’objet d’une déclaration 1656629 auprès de la Commission nationale de l’informatique et des libertés <a href="#">www.cnilfr</a></p>
+            <h3>
+                <li>Litiges :</li>
+            </h3>
+            <p>Les présentes conditions du site sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.</p>
+            <h3>
+                <li>Données personnelles :
+            </h3>
+            <p>De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a href="index.php"></a>
+                Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « <a href="footer.php">contact</a> ».
+                Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, notamment celui de solliciter des renseignements sur notre société, ou de recevoir les lettres d’information.
+                Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d’accès.
+                De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
+            <p> cliquer <a href="mailto: bertolucci@gmail.com">ici </a>pour nous envoyer un email </p>
+    </div>
+    </ol>
+</main>
+<?php get_footer(); ?>
