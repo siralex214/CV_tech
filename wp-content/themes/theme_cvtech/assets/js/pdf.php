e382a67e8c1c9d9b75037c2b1c0e7632c5b072a8
Hyotre <72597664+Hyotre1@users.noreply.github.com>
2022-03-16 08:58:27 +0100

    pdf

---
+++ wp-content/themes/theme_cvtech/assets/js/pdf.php
@@ -0,0 +1,12 @@
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
+
+    <div>
+        <div>
+            <div>
+                <button class="btn btn-primary" id="download"> télécharger en pdf</button>
+            </div>
+            <div>
+                <div class="card" id="invoice">
+                    <h1 style="color:red;">Bonjoir toi</h1>
+                </div>
