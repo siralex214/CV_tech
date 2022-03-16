ab5a8c19560a0b638d2b7e7e3c0eac3f4ee8c2f1
Hyotre <72597664+Hyotre1@users.noreply.github.com>
2022-03-16 11:29:46 +0100

    branch switch

---
+++ wp-content/themes/theme_cvtech/template_recruteur.php
@@ -0,0 +1,13 @@
+<?php 
+/* Template Name: recruteur */
+get_header();
+?>
+<main>
+<div class="recruteur-bordure">
+<button class="btn btn-primary">voir le profil</button>
+    <div>   
+            <button class="btn btn-primary" id="download"> télécharger en pdf</button> 
+    </div>
+</div>
+</main>
+<?php get_footer(); ?>
