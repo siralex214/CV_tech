787c7316c51751d5871bdbaf69e49f5b2ea16dfd
Hyotre <72597664+Hyotre1@users.noreply.github.com>
2022-03-16 13:59:19 +0100

    feature_recruteur

--- wp-content/themes/theme_cvtech/template_recruteur.php
+++ wp-content/themes/theme_cvtech/template_recruteur.php
@@ -1,13 +1,41 @@
-<?php 
+<?php
 /* Template Name: recruteur */
 get_header();
 ?>
 <main>
-<div class="recruteur-bordure">
-<button class="btn btn-primary">voir le profil</button>
-    <div>   
-            <button class="btn btn-primary" id="download"> télécharger en pdf</button> 
-    </div>
-</div>
+    <div class="recruteur-main">
+        <div class="recruteur-bordure">
+            <div class="recruteur-button">affichage du profil blabla</div>
+            <div class="recruteur-milieu">affichage du profil blabla</div>
+            <div class="recruteur-right">
+
+                <button class="btn btn-primary">envoyer un mail</button>
+                <div class="recruteur-horizontal">
+                    <button class="btn btn-primary" id="download"> télécharger en pdf</button>
+                </div>
+            </div>
+        </div>
+        <div class="recruteur-bordure">
+        <div class="recruteur-button">affichage du profil blabla</div>
+            <div class="recruteur-milieu">affichage du profil blabla</div>
+            <div class="recruteur-right">
+
+                <button class="btn btn-primary">envoyer un mail</button>
+                <div class="recruteur-horizontal">
+                    <button class="btn btn-primary" id="download"> télécharger en pdf</button>
+                </div>
+            </div>
+        </div>
+        <div class="recruteur-bordure">
+        <div class="recruteur-button">affichage du profil blabla</div>
+            <div class="recruteur-milieu">affichage du profil blabla</div>
+            <div class="recruteur-right">
+
+                <button class="btn btn-primary">envoyer un mail</button>
+                <div class="recruteur-horizontal">
+                    <button class="btn btn-primary" id="download"> télécharger en pdf</button>
+                </div>
+            </div>
+        </div>
 </main>
 <?php get_footer(); ?>
