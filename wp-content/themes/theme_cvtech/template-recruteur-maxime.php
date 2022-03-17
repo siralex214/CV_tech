34ab06d72edaf4665977cb35cb1cdd0dd109d586
Maxime Hébert <83720884+MaximeMartz@users.noreply.github.com>
2022-03-17 13:43:28 +0100

    template recruteur + css

---
+++ wp-content/themes/theme_cvtech/template-recruteur-maxime.php
@@ -0,0 +1,112 @@
+<?php
+/*Template Name: Recruteur-Maxime*/
+get_header();
+?>
+
+<body>
+
+    <main id="main-recruteur">
+
+        <div class="candidat-profil-container">
+            <div class="candidat-profil-infos-img">
+                <img class="candidat-profil-img" src="dalecooper.jpg">
+            </div>
+            <div class="candidat-profil-infos-text">
+                <span class="candidat-profil-name">Maxime Hébert</span>
+                <span class="candidat-profil-title">Développeur Web</span>
+                <span class="candidat-profil-location"><i class="fa-solid fa-location-dot"></i> Rouen, Normandie</span>
+            </div>
+            <div class="candidat-profil-skills">
+                <ul>
+                    <li class="skills-container">HTML</li>
+                    <li class="skills-container">CSS</li>
+                    <li class="skills-container">JavaScript</li>
+                    <li class="skills-container">PHP</li>
+                    <li class="skills-container">Wordpress</li>
+                </ul>
+            </div>
+            <div class="candidat-profil-links">
+                <button class="button-recruteur">Télécharger CV</button>
+                <button class="button-recruteur button-contact">Contacter</button>
+            </div>
+        </div>
+
+        <div class="candidat-profil-container">
+            <div class="candidat-profil-infos-img">
+                <img class="candidat-profil-img" src="alexis.jpg">
+            </div>
+            <div class="candidat-profil-infos-text">
+                <span class="candidat-profil-name">Alexis Mouchon</span>
+                <span class="candidat-profil-title">Développeur React</span>
+                <span class="candidat-profil-location"><i class="fa-solid fa-location-dot"></i> Rouen, Normandie</span>
+            </div>
+            <div class="candidat-profil-skills">
+                <ul>
+                    <li class="skills-container">React</li>
+                    <li class="skills-container">CSS</li>
+                    <li class="skills-container">JavaScript</li>
+                    <li class="skills-container">PHP</li>
+                    <li class="skills-container">Wordpress</li>
+                </ul>
+            </div>
+            <div class="candidat-profil-links">
+                <button class="button-recruteur">Télécharger CV</button>
+                <button class="button-recruteur button-contact">Contacter</button>
+            </div>
+        </div>
+
+        <div class="candidat-profil-container">
+            <div class="candidat-profil-infos-img">
+                <img class="candidat-profil-img" src="marcus.jpg">
+            </div>
+            <div class="candidat-profil-infos-text">
+                <span class="candidat-profil-name">Marcus Mendy</span>
+                <span class="candidat-profil-title">Développeur Frontend</span>
+                <span class="candidat-profil-location"><i class="fa-solid fa-location-dot"></i> Rouen, Normandie</span>
+            </div>
+            <div class="candidat-profil-skills">
+                <ul>
+                    <li class="skills-container">HTML</li>
+                    <li class="skills-container">CSS</li>
+                    <li class="skills-container">JavaScript</li>
+                    <li class="skills-container">PHP</li>
+                    <li class="skills-container">Wordpress</li>
+                </ul>
+            </div>
+            <div class="candidat-profil-links">
+                <button class="button-recruteur">Télécharger CV</button>
+                <button class="button-recruteur button-contact">Contacter</button>
+            </div>
+        </div>
+
+        <div class="candidat-profil-container">
+            <div class="candidat-profil-infos-img">
+                <img class="candidat-profil-img" src="dalecooper.jpg">
+            </div>
+            <div class="candidat-profil-infos-text">
+                <span class="candidat-profil-name">Maxime Hébert</span>
+                <span class="candidat-profil-title">Développeur Web</span>
+                <span class="candidat-profil-location"><i class="fa-solid fa-location-dot"></i> Rouen, Normandie</span>
+            </div>
+            <div class="candidat-profil-skills">
+                <ul>
+                    <li class="skills-container">HTML</li>
+                    <li class="skills-container">CSS</li>
+                    <li class="skills-container">JavaScript</li>
+                    <li class="skills-container">PHP</li>
+                    <li class="skills-container">Wordpress</li>
+                </ul>
+            </div>
+            <div class="candidat-profil-links">
+                <button class="button-recruteur">Télécharger CV</button>
+                <button class="button-recruteur button-contact">Contacter</button>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+
+<?php
+get_footer();
+?>
