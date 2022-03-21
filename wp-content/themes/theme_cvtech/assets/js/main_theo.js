b641ed5ad63b8caf33212527b6275284cdc1b9c8
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-03-21 17:01:59 +0100

    idée js pdf(test)

--- wp-content/themes/theme_cvtech/assets/js/main_theo.js
+++ wp-content/themes/theme_cvtech/assets/js/main_theo.js
@@ -4,13 +4,12 @@ window.onload = function () {
     console.log(invoice);
     console.log(window);
     var opt = {
-      margin: -25,
-      padding: -30,
+      margin: [-20, -30, -100, -30],
       filename: "myfile.pdf",
       image: { type: "png", quality: 0.98 },
       html2canvas: { scale: 2 },
 
-      jsPDF: { unit: "mm", format: "A4", orientation: "portrait" },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
     };
     html2pdf().from(invoice).set(opt).save();
   });
