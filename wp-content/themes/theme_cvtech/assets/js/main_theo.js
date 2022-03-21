c389fe0bf098f572794688cc71fab145d26bc194
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-03-21 16:36:51 +0100

    modif exit pdf

--- wp-content/themes/theme_cvtech/assets/js/main_theo.js
+++ wp-content/themes/theme_cvtech/assets/js/main_theo.js
@@ -1,16 +1,17 @@
-window.onload = function() {
-    document.getElementById("download")
-        .addEventListener("click", () => {
-            const invoice = this.document.getElementById("invoice");
-            console.log(invoice);
-            console.log(window);
-            var opt = {
-                margin: 1,
-                filename: 'myfile.pdf',
-                image: { type: 'jpeg', quality: 0.98 },
-                html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-            };
-            html2pdf().from(invoice).set(opt).save();
-        })
-}
+window.onload = function () {
+  document.getElementById("download").addEventListener("click", () => {
+    const invoice = this.document.getElementById("invoice");
+    console.log(invoice);
+    console.log(window);
+    var opt = {
+      margin: -25,
+      padding: -30,
+      filename: "myfile.pdf",
+      image: { type: "png", quality: 0.98 },
+      html2canvas: { scale: 2 },
+
+      jsPDF: { unit: "mm", format: "A4", orientation: "portrait" },
+    };
+    html2pdf().from(invoice).set(opt).save();
+  });
+};
