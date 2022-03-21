bf7aa25d1b6cbbe0ba4bd53219579e3fdc42d783
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-03-21 15:09:09 +0100

    avant merge

--- wp-content/themes/theme_cvtech/template_pdf.php
+++ wp-content/themes/theme_cvtech/template_pdf.php
@@ -6,11 +6,16 @@ get_header();
 <div>
     <div>
         <div>
-            <button class="btn btn-primary" id="download"> télécharger en pdf</button>
-        </div>
-        <div>
-            <div class="card" id="invoice">
-                <h1 style="color:red;">Bonjoir toi</h1>
+            <div class="bodypdf">
+                <div class="card" id="invoice">
+                    <div class="carreun">a</div>
+                    <div class="carredeux">
+                        <h1 class="title_pdf">wesh alors</h1>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn btn-primary" id="download"> télécharger en pdf</button>
+                </div>
             </div>
         </div>
     </div>
