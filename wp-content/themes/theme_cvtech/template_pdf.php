70653637aa971be8babe774dc8c2be16ea3ac112
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-03-23 17:06:53 +0100

    push page cv

--- wp-content/themes/theme_cvtech/template_pdf.php
+++ wp-content/themes/theme_cvtech/template_pdf.php
@@ -1,6 +1,5 @@
 <?php
 /* Template Name: pdf */
-get_header();
 
 ?>
 <?php
@@ -40,9 +39,9 @@ $info_persos['permis'] = explode("; ", $info_persos['permis']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="_fabien.css">
+    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/assets/css/style_fabien.css">
     <title>Document</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js">
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" defer></script>
     <script src="<?= get_stylesheet_directory_uri() ?>/assets/js/main_theo.js" defer></script>
 </head>
 
@@ -56,29 +55,27 @@ $info_persos['permis'] = explode("; ", $info_persos['permis']);
                 <div class="card" id="invoice">
                     <div class="left-column">
                         <div>
-                            <label for="nom">Nom:</label>
-                            <input type="nom" name="nom" value="<?= $info_persos['prenom'] ?>">
+                            <p type="nom" name="nom"><?= $info_persos['prenom'] ?></p>
                         </div>
                         <br>
                         <div>
-                            <label for="prenom">Prénom:</label>
-                            <input type="prenom" name="prenom" value="<?= $info_persos['nom'] ?>">
+                            <p type="prenom" name="prenom"><?= $info_persos['nom'] ?></p>
                         </div>
                         <br>
-                        <div> <label for="email">Adresse E-mail:</label>
-                            <input type="email" name="email" value="<?= $info_persos['email'] ?>">
+                        <div>
+                            <p type="email" name="email"><?= $info_persos['email'] ?></p>
                         </div>
                         <br>
-                        <div> <label for="date">date de naissance:</label>
-                            <input type="date" name="date" value="<?= $info_persos['date_de_naissance'] ?>">
+                        <div>
+                            <p type="date" name="date"><?= $info_persos['date_de_naissance'] ?></p>
                         </div>
                         <br>
-                        <div> <label for="num"> numéro de telephone:</label>
-                            <input type="num" name="num" value="<?= $info_persos['telephone'] ?>">
+                        <div>
+                            <p type="num" name="num"><?= $info_persos['telephone'] ?></p>
                         </div>
                         <br>
-                        <div> <label for="adresse">adresse postale:</label>
-                            <input type="adresse" name="adresse" value="<?= $info_persos['addresse_postale'] ?>">
+                        <div>
+                            <p type="adresse" name="adresse"><?= $info_persos['addresse_postale'] ?></p>
                         </div>
                         <br>
                         <div> <label for="permis"> permis:</label>
@@ -88,20 +85,19 @@ $info_persos['permis'] = explode("; ", $info_persos['permis']);
                         </div>
                         <div>
                             <div>
-                                <label for="competence">les competences:</label>
-                                <input type="competence" name="competence" value="<?= $info_persos['competences'] ?>">
+                                <p type="competence" name="competence"><?= $info_persos['competences'] ?></p>
                             </div>
                             <br>
                             <br>
                             <div>
-                                <label for="langues">les langues:</label>
-                                <input type="langues" name="langues" value="<?= $info_persos['langues'] ?>">
+
+                                <p type="langues" name="langues"><?= $info_persos['langues'] ?></p>
                             </div>
                             <br>
                             <br>
                             <div>
                                 <label for="hobbies">les hobbies:</label>
-                                <input type="hobbies" name="hobbies" value="<?= $info_persos['hobbies'] ?>">
+                                <p type="hobbies" name="hobbies"><?= $info_persos['hobbies'] ?></p>
                             </div>
                         </div>
                     </div>
@@ -109,17 +105,17 @@ $info_persos['permis'] = explode("; ", $info_persos['permis']);
                         <div class="header">
                             <div>
                                 <label for="entreprise">l'entreprise:</label>
-                                <input type="entreprise" name="entreprise" value="<?= $exp_pros['entreprise'] ?>">
+                                <p type="entreprise" name="entreprise"><?= $exp_pros['entreprise'] ?></p>
                             </div>
                             <br>
                             <div>
                                 <label for="lieu">lieu:</label>
-                                <input type="lieu" name="lieu" value="<?= $exp_pros['lieu'] ?>">
+                                <p type="lieu" name="lieu"><?= $exp_pros['lieu'] ?></p>
                             </div>
                             <br>
                             <div>
                                 <label for="poste">Poste:</label>
-                                <input type="poste" name="poste" value="<?= $exp_pros['poste'] ?>">
+                                <p type="poste" name="poste"><?= $exp_pros['poste'] ?></p>
                             </div>
                             <br>
                             <div>
@@ -130,25 +126,25 @@ $info_persos['permis'] = explode("; ", $info_persos['permis']);
                                 <?php endforeach; ?>
                             </div>
                         </div>
-                        <div class=" content">
+                        <div class=" contentpdf">
                             <div>
                                 <label for="etablissement">etablissement:</label>
-                                <input type="etablissement" name="etablissement" value="<?= $formations['etablissement'] ?>">
+                                <p type="etablissement" name="etablissement"><?= $formations['etablissement'] ?></p>
                             </div>
                             <br>
                             <div>
                                 <label for="diplome">diplome:</label>
-                                <input type="diplome" name="diplome" value="<?= $formations['diplome'] ?>">
+                                <p type="diplome" name="diplome"><?= $formations['diplome'] ?></p>
                             </div>
                             <br>
                             <div>
                                 <label for="lieu">lieu:</label>
-                                <input type="lieu" name="lieu" value="<?= $formations['lieu'] ?>">
+                                <p type="lieu" name="lieu"><?= $formations['lieu'] ?></p>
                             </div>
                             <br>
                             <div>
                                 <label for="poste">titre:</label>
-                                <input type="titre" name="titre" value="<?= $formations['titre'] ?>">
+                                <p type="titre" name="titre"><?= $formations['titre'] ?></p>
                             </div>
                         </div>
                     </div>
